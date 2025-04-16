fdf972a3b68d80d9bafe33d4e67c27a48467a8cb
Mariano Villarreal <marianov994@gmail.com>
2025-04-16 18:32:22 -0300

    add html to post

--- src/components/AdminPostFeed.tsx
+++ src/components/AdminPostFeed.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import Link from "next/link";
@@ -8,7 +9,19 @@ import PostImages from "@/components/PostImages";
 import { useUserProfileByUsername } from "@/lib/hooks";
 
 import { DocumentData } from "firebase/firestore";
-import { HeartIcon, PencilIcon } from "@heroicons/react/24/outline";
+import {
+  HeartIcon,
+  PencilIcon,
+  ChatBubbleLeftIcon,
+  ArrowsRightLeftIcon,
+} from "@heroicons/react/24/outline";
+
+import "./AdminPostFeed.css";
+
+// TODO: Add the images handling (see PostImages.tsx)
+import feet from "./../public/images/feet.jpg";
+import vampire from "./../public/images/vapire.png";
+import riko from "./../public/images/riko2.jpeg";
 
 interface IAdminPostFeedProps {
   posts: DocumentData[];
@@ -35,40 +48,111 @@ const AdminPostItem = ({ post }: IAdminPostItemProps) => {
   };
 
   return (
-    <div className="bg-white p-4 mb-4 rounded-lg shadow" data-testid={`post-${post.id}`}>
-      <div className="flex justify-between items-center mb-2">
-        <button onClick={onGoToUserProfile}>
-          <h2 className="text-md font-bold" data-testid="post-author">
-            By @{post.username}
-          </h2>
-        </button>
-        <Link href={`/private/posts/${post.username}/${post.postId}`} passHref>
-          <span
-            className={`text-sm font-semibold ${post.published ? "text-black" : "text-gray-500"}`}
-            data-testid="post-status"
-          >
-            {post.published ? "Activo" : "Borrador"}
-          </span>
-        </Link>
-      </div>
+    <div className="post" data-testid={`post-${post.id}`}>
+      <div className="post-header">
+        <div className="post-header-content">
+          <div className="top-line">
+            <div className="user-block">
+              <img src={vampire.src} alt="Avatar" className="avatar" />
+            </div>
 
-      <Link href={`/private/posts/${post.username}/${post.postId}`}>
-        <ReactMarkdown className="text-gray-700" data-testid="post-content">
-          {post?.content}
-        </ReactMarkdown>
-        <PostImages images={post.images || []} />
-      </Link>
+            <div className="contenedorcito">
+              <button onClick={onGoToUserProfile}>
+                <span className="username">{post.username}</span>
+                <span className="handle">By @{post.username}</span>
+              </button>
+            </div>
 
-      <div className="flex justify-between items-center mt-4">
-        <div className="flex items-center text-gray-600 text-sm" data-testid="post-hearts">
-          <HeartIcon className="h-6 w-6 mr-1" />
-          <span>{post.hearts || 0} Likes</span>
+            <div className="time-block">
+              <span className="time">2 hours ago ...</span>
+            </div>
+          </div>
+
+          <div className="post-content">
+            <Link href={`/private/posts/${post.username}/${post.postId}`}>
+              <ReactMarkdown
+                className="text-gray-700"
+                data-testid="post-content"
+              >
+                {post?.content}
+              </ReactMarkdown>
+              {post.images && <PostImages images={post.images || []} />}
+            </Link>
+          </div>
+
+          <div className="related-profiles">
+            {/* TODO: Add the images handling (see PostImages.tsx) */}
+            <div className="profile-card">
+              <div
+                className="profile-card-bg"
+                style={{ backgroundImage: `url(${feet.src})` }}
+              ></div>
+              <div className="profile-card-content">
+                <img
+                  className="card-avatar"
+                  src={vampire.src}
+                  alt="Hagen Avatar"
+                />
+
+                <div className="card-text">
+                  <span className="profile-name">Vampire ✔️</span>
+                  <span className="profile-handle">@Vampire.AI</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="profile-card">
+              <div
+                className="profile-card-bg"
+                style={{ backgroundImage: `url(${riko.src})` }}
+              ></div>
+              <div className="profile-card-content">
+                <img className="card-avatar" src={riko.src} alt="OFTV Logo" />
+                <div className="card-text">
+                  <span className="profile-name">Riko ✔️</span>
+                  <span className="profile-handle">@Riko.L</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="post-actions flex justify-between items-center mt-4">
+            <button
+              onClick={() => {}}
+              className="flex items-center text-gray-600 text-sm"
+              data-testid="post-hearts"
+            >
+              <HeartIcon className="h-6 w-6 mr-1" />
+              <span>{post.hearts || 0}</span>
+            </button>
+
+            <button
+              onClick={() => {}}
+              className="flex items-center text-gray-600 text-sm"
+              data-testid="post-comments"
+            >
+              <ChatBubbleLeftIcon className="h-6 w-6 mr-1" />
+              <span>{post.commentsCount || 0}</span>
+            </button>
+
+            <button
+              onClick={() => {}}
+              className="text-gray-600 hover:text-gray-900"
+              data-testid="post-retweet"
+            >
+              <ArrowsRightLeftIcon className="h-6 w-6" />
+            </button>
+
+            <Link href={`/posts/edit/${post.postId}`}>
+              <button
+                className="text-gray-600 hover:text-gray-900"
+                data-testid="post-pin"
+              >
+                <PencilIcon className="h-6 w-6" />
+              </button>
+            </Link>
+          </div>
         </div>
-        <Link href={`/posts/edit/${post.postId}`} passHref>
-          <button className="text-gray-600 hover:text-gray-900" data-testid="post-edit">
-            <PencilIcon className="h-6 w-6" />
-          </button>
-        </Link>
       </div>
     </div>
   );
