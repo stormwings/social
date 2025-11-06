1fa6ae3cc30fc2b9b8470fa7ec5a3a4d850b8bd6
Mariano Villarreal <marianov994@gmail.com>
2025-11-06 12:08:17 -0300

    Implements sign-in with wallet and Google
    Adds sign-in with wallet and Google functionalities
    
    Introduces username creation form after successful sign-in
    
    Refactors deposit page UI

--- src/app/deposit/page.tsx
+++ src/app/deposit/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useEffect, useState } from "react";
 
 import { ClipboardIcon, CheckIcon } from "@heroicons/react/24/outline";
@@ -12,88 +11,83 @@ import TopbarMedia from "@/components/TopbarMedia";
 import Utils from "@/lib/utils";
 
 export default function Deposit() {
-    const { ethereumAddress } = useUserData();
-    const [countdown, setCountdown] = useState(false);
+  const { ethereumAddress } = useUserData();
+  const [countdown, setCountdown] = useState(false);
 
-    useEffect(() => {
-        if (countdown) {
-            const timer = setTimeout(() => setCountdown(false), 1000);
-            return () => clearTimeout(timer);
-        }
-    }, [countdown]);
+  useEffect(() => {
+    if (countdown) {
+      const timer = setTimeout(() => setCountdown(false), 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [countdown]);
 
-    return (
-        <div className="min-h-screen">
-            <TopbarMedia title="Deposit" showBack />
-            <div className="max-w-sm mx-auto mt-8">
-                <div className="mb-4 p-4 bg-gray-100 border border-gray-300 rounded-lg">
-                    <p className="text-gray-700 text-sm font-semibold">
-                        Deposit USDT
-                    </p>
+  return (
+    <div className="min-h-screen">
+      <TopbarMedia title="Deposit" showBack />
+      <div className="max-w-sm mx-auto mt-8">
+        <div className="mb-4 p-4 bg-gray-100 border border-gray-300 rounded-lg">
+          <p className="text-gray-700 text-sm font-semibold">Deposit USDT</p>
 
-                    <div className="flex justify-center p-6">
-                        <QRCode
-                            bgColor="#F3F4F6"
-                            fgColor="#374151"
-                            size={256}
-                            value={ethereumAddress || ""}
-                            viewBox={`0 0 256 256`}
-                        />
-                    </div>
+          <div className="flex justify-center p-6">
+            <QRCode
+              bgColor="#F3F4F6"
+              fgColor="#374151"
+              size={256}
+              value={ethereumAddress || ""}
+              viewBox={`0 0 256 256`}
+            />
+          </div>
 
-                    <div className="flex items-center bg-white p-2 rounded-full">
-                        <input
-                            onClick={() => {
-                                Utils.copyToClipboard(ethereumAddress || "");
-                                setCountdown(true);
-                            }}
-                            className="flex-grow pl-2 bg-transparent outline-none"
-                            type="text"
-                            value={
-                                ethereumAddress
-                                    ? Utils.shortenAddress(ethereumAddress, 15, 12)
-                                    : "Loading..."
-                            }
-                            readOnly
-                        />
-                        {countdown ? (
-                            <CheckIcon className="h-5 w-5 text-gray-700 cursor-pointer" />
-                        ) : (
-                            <ClipboardIcon
-                                className="h-5 w-5 text-gray-700 cursor-pointer"
-                                onClick={() => {
-                                    Utils.copyToClipboard(ethereumAddress || "");
-                                    setCountdown(true);
-                                }}
-                            />
-                        )}
-                    </div>
+          <div className="flex items-center bg-white p-2 rounded-full">
+            <input
+              onClick={() => {
+                Utils.copyToClipboard(ethereumAddress || "");
+                setCountdown(true);
+              }}
+              className="flex-grow pl-2 bg-transparent outline-none"
+              type="text"
+              value={
+                ethereumAddress
+                  ? Utils.shortenAddress(ethereumAddress, 15, 12)
+                  : "Loading..."
+              }
+              readOnly
+            />
+            {countdown ? (
+              <CheckIcon className="h-5 w-5 text-gray-700 cursor-pointer" />
+            ) : (
+              <ClipboardIcon
+                className="h-5 w-5 text-gray-700 cursor-pointer"
+                onClick={() => {
+                  Utils.copyToClipboard(ethereumAddress || "");
+                  setCountdown(true);
+                }}
+              />
+            )}
+          </div>
 
-                    <div
-                        onClick={() => {
-                            Utils.copyToClipboard(ethereumAddress || "");
-                            setCountdown(true);
-                        }}
-                        className="flex justify-center p-2 rounded-full"
-                    >
-                        <p className="text-xs text-gray-700">Copy Address</p>
-                    </div>
-                </div>
-
-                <div className="mb-4 p-4 border rounded-lg">
-                    <div className="p-4">
-                        <p className="text-gray-700 font-semibold mb-2">
-                            Deposit USDT or USDC
-                        </p>
-                        <p className="text-gray-500 text-xs mb-4">
-                            Link your ERC20 wallet and easily to convert.
-                        </p>
-                    </div>
-                    <button className="block w-full bg-gray-100 text-gray-700 py-2 mb-1 rounded-lg font-semibold text-center">
-                        Deposit
-                    </button>
-                </div>
-            </div>
+          <div
+            onClick={() => {
+              Utils.copyToClipboard(ethereumAddress || "");
+              setCountdown(true);
+            }}
+            className="flex justify-center p-2 rounded-full"
+          >
+            <p className="text-xs text-gray-700">Copy Address</p>
+          </div>
         </div>
-    );
+
+        <div className="mb-4 p-4 border rounded-lg">
+          <div className="p-4">
+            <p className="text-gray-700 font-semibold mb-2">
+              Deposit USDT or USDC
+            </p>
+            <p className="text-gray-500 text-xs">
+              Use your ERC20 wallet and easily to convert.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,146 +1,94 @@
 "use client";
 
-import { useContext, useEffect, useState } from "react";
-import { UserContext, UserContextType } from "@/lib/context";
+import { memo, useContext, useEffect, useCallback, useState } from "react";
 import { useRouter } from "next/navigation";
 import { WalletIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
-import googleImg from "../../public/google.png";
 import { getAuth } from "firebase/auth";
 
-import {
-  useSignInWithWallet,
-  useSignInWithGoogle,
-  useUsernameValidation,
-  useSubmitUsername,
-} from "@/lib/hooks";
+import { Button } from "@/components/dumb/Button";
+import FormUsernameCreate from "@/components/smart/FormUsernameCreate";
 
-const SignInWalletButton = ({
+import { useSignInWithWallet, useSignInWithGoogle } from "@/lib/hooks";
+import { UserContext, UserContextType } from "@/lib/context";
+
+import googleImg from "./../../public/google.png";
+
+type SaveUserWallet = (wallet: string | null) => void;
+
+const ROUTES = { HOME: "/private/home" } as const;
+
+const SignInWalletButton = memo(function SignInWalletButton({
   saveUserWallet,
 }: {
-  saveUserWallet: Function;
-}) => {
+  saveUserWallet: SaveUserWallet;
+}) {
   const { signInWithWallet } = useSignInWithWallet();
+  const onClick = useCallback(
+    () => signInWithWallet(saveUserWallet),
+    [signInWithWallet, saveUserWallet]
+  );
 
   return (
-    <button
-      className="text-primary font-bold py-3 px-6 rounded-md text-center border border-primary flex items-center justify-center"
-      onClick={() => signInWithWallet(saveUserWallet)}
+    <Button
+      variant="outline"
+      leftIcon={<WalletIcon width={20} height={20} className="h-5 w-5" />}
+      onClick={onClick}
+      aria-label="Sign in with Ethereum wallet"
     >
-      <WalletIcon width={24} height={24} className="h-5 w-5 text-primary" />
-      <span className="pl-2">Sign in with Wallet</span>
-    </button>
+      Sign in with Wallet
+    </Button>
   );
-};
+});
 
-const SignInButton = () => {
+const SignInGoogleButton = memo(function SignInGoogleButton() {
   const { signInWithGoogle } = useSignInWithGoogle();
-
   return (
-    <button
-      className="text-primary font-bold py-3 px-6 rounded-md text-center border border-primary flex items-center justify-center"
+    <Button
+      variant="outline"
+      leftIcon={
+        <Image src={googleImg} alt="Google logo" width={20} height={20} />
+      }
       onClick={signInWithGoogle}
+      aria-label="Sign in with Google"
     >
-      <Image
-        src={googleImg}
-        alt="Google Logo"
-        width={20}
-        height={20}
-        className="mr-2"
-      />
-      <span>Sign in with Google</span>
-    </button>
+      Sign in with Google
+    </Button>
   );
-};
+});
 
 export default function Enter() {
   const { user, username } = useContext(UserContext) as UserContextType;
-  const [userWallet, setUserWallet] = useState(null);
+  const [userWallet, setUserWallet] = useState<string | null>(null);
   const router = useRouter();
 
-  const auth = getAuth();
-  auth.useDeviceLanguage();
+  useEffect(() => {
+    const auth = getAuth();
+    auth.useDeviceLanguage();
+  }, []);
 
   useEffect(() => {
-    if (router && user && username) {
-      router.push("/private/home");
+    if (user && username) {
+      router.replace(ROUTES.HOME);
     }
   }, [user, username, router]);
+
+  const shouldShowUsernameForm = !!user && !username;
+  const shouldShowSignIn = !user;
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
       <div className="w-full flex flex-col items-center space-y-4 pb-8 mt-32">
-        {user ? (
-          !username ? (
-            <UsernameForm userWallet={userWallet} />
-          ) : null
-        ) : (
+        {shouldShowUsernameForm && (
+          <FormUsernameCreate userWallet={userWallet} />
+        )}
+        {shouldShowSignIn && (
           <>
             <SignInWalletButton saveUserWallet={setUserWallet} />
-            <SignInButton />
+            <SignInGoogleButton />
           </>
         )}
       </div>
     </div>
   );
 }
-
-const UsernameForm = ({ userWallet }: { userWallet: any }) => {
-  const { user, username } = useContext(UserContext) as UserContextType;
-  const { formValue, isValid, loading, handleChange } = useUsernameValidation();
-  const { submitUsername } = useSubmitUsername(user, userWallet);
-
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await submitUsername(formValue);
-  };
-
-  return !username ? (
-    <>
-      <h3 className="text-primary text-2xl font-semibold mb-4">
-        Choose Your Unique Identity
-      </h3>
-      <form onSubmit={onSubmit} className="space-y-4">
-        <input
-          name="username"
-          placeholder="Username"
-          value={formValue}
-          onChange={handleChange}
-          className="p-2 w-full border border-gray-300 rounded-lg bg-white"
-        />
-        <UsernameMessage
-          username={formValue}
-          isValid={isValid}
-          loading={loading}
-        />
-        <button
-          type="submit"
-          className="bg-primary text-black px-4 py-2 rounded disabled:opacity-50 w-full"
-          disabled={!isValid || loading}
-        >
-          {loading ? "Processing..." : "Confirm"}
-        </button>
-      </form>
-    </>
-  ) : null;
-};
-
-const UsernameMessage = ({
-  username,
-  isValid,
-  loading,
-}: {
-  username: string;
-  isValid: boolean;
-  loading: boolean;
-}) => {
-  if (loading) return <p className="text-yellow-500 mb-2">Verifying...</p>;
-
-  if (isValid)
-    return <p className="text-green-500 mb-2">{username} is available!</p>;
-
-  if (username && !isValid)
-    return <p className="text-red-500 mb-2">{username} is not available</p>;
-
-  return null;
-};
